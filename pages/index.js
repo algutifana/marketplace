466f638d43d9845e43f82446c4f1aa354e86de98
algutifana <aaron.algutifan@gmail.com>
2022-03-29 22:51:58 -0500

    Made NFT zoom on hover

--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,7 @@ export default function Home() {
         <div className = "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4 ">
           {
             nfts.map((nft, i) =>(
-              <div key = {i} className = "border shadow rounded-xl overflow-hidden">
+              <div key = {i} className = "zoom border shadow rounded-xl overflow-hidden">
                 <Image src = {nft.image} alt = "NFT Image" width = {300} height = {300}/>
                 <div className = "p-4">
                   <p style = {{height: '50px', textAlign: 'center'}}className = "text-2xl text-thelinks">
